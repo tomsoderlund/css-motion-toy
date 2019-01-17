c83373af39e08d8a9a9cb13400165e752ee2e6e2
Tom Söderlund <tom@tomsoderlund.com>
2019-01-17 10:23:15 +0100

    Simulator placeholder

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from '../images/logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import Simulator from '../components/Simulator'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <h1>CSS Motion Toy</h1>
+        <Simulator />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/Simulator.js
+++ src/components/Simulator.js
@@ -1,16 +1,66 @@
-import { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+
+import styled from 'styled-components'
+
+const SimulatorContainer = styled.div`
+  position: relative;
+  display: inline-block;
+  width: 20em;
+  height: 20em;
+  background-color: #eee;
+  border: 1px solid gray;
+`
+
+const TheBox = styled.div`
+  position: absolute;
+  width: 10em;
+  height: 5em;
+  background-color: slateblue;
+  border-radius: 0.5em;
+`
+
+const TIMER_INTERVAL = 50
 
 export default class Simulator extends Component {
-
   constructor (props) {
     super(props)
-    this.state = { propName: 'this prop' }
+    this.state = {
+      isRunning: false,
+      timeStarted: 0,
+      elapsedTime: 0
+    }
+  }
+
+  componentDidMount () {
+    this.timer = setInterval(this.onTimerUpdate.bind(this), TIMER_INTERVAL)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
+  onTimerUpdate () {
+    const elapsedTime = Date.now() - this.state.timeStarted
+    this.setState({ elapsedTime })
+  }
+
+  toggleRunning () {
+    const isRunning = !this.state.isRunning
+    this.setState({ isRunning })
+    if (isRunning) {
+      const timeStarted = Date.now()
+      this.setState({ timeStarted })
+    }
   }
 
   render () {
-    return <div>
-      Simulator: {this.props.propName}
-    </div>
+    return <Fragment>
+      <SimulatorContainer>
+        <TheBox />
+      </SimulatorContainer>
+      <br />
+      Time: {this.state.isRunning ? this.state.elapsedTime : '-'}
+      <button onClick={this.toggleRunning.bind(this)}>{!this.state.isRunning ? 'Start' : 'Stop'}</button>
+    </Fragment>
   }
-
 }
